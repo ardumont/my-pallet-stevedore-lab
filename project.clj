2daebf925839add8918dbcf24f522b8a2b3d65b3
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-29 21:33:42 +0200

    Update the dependencies with stevedore/pallet as the framework to play with and midje as the testing framework.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject my-pallet-stevedore-lab "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"] [org.cloudhoist/stevedore "0.7.0"]]
+  :dev-dependencies [[midje      "1.2.0"]
+                     [lein-midje "1.0.3"]]
+  :repositories {"sonatype"
+                 "http://oss.sonatype.org/content/repositories/releases"})
