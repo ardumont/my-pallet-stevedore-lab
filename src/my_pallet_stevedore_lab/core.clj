d40186bfafbec804db14139a5ebb92aff10678e2
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-30 12:11:59 +0100

    First test to generate some bash shell.

--- src/my_pallet_stevedore_lab/core.clj
+++ src/my_pallet_stevedore_lab/core.clj
@@ -1,1 +1,31 @@
-(ns my-pallet-stevedore-lab.core)
+(ns my-pallet-stevedore-lab.core
+  (:use [clojure.test               :only [run-tests]])
+  (:use [midje.sweet])
+  (:use [clojure.contrib.repl-utils :only [show]])
+  (:use [clojure.pprint             :only [pprint]])
+  (:use [clojure.walk               :only [macroexpand-all]])
+  (:use
+   [pallet.common.string :only [quoted]]
+   pallet.stevedore
+   clojure.test)
+  (:require
+   [pallet.script :as script]
+   [pallet.stevedore.common]
+   [pallet.stevedore.bash]
+   [pallet.common.filesystem :as filesystem]
+   [pallet.common.logging.logutils :as logutils]
+   [pallet.common.shell :as shell]
+   [slingshot.core :as slingshot]
+   [clojure.tools.logging :as logging]))
+
+; try and learn how to play with stevedore
+; https://github.com/pallet/stevedore/blob/develop/test/pallet/stevedore/bash_test.clj
+
+(fact
+  (with-script-language :pallet.stevedore.bash/bash
+    (script (if (= foo bar) (println fred)))) => "if [ \\( \"foo\" == \"bar\" \\) ]; then echo fred;fi")
+
+;.;. Happiness comes when you believe that you have done something truly
+;.;. meaningful. -- Yan
+(fact
+  (with-script-language :pallet.stevedore.bash/bash (script (if (file-exists? "~/bin/cake") (println (quoted "cool"))))) => "if [ -e ~/bin/cake ]; then echo \"cool\";fi")
